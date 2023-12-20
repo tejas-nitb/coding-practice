d3e48382c6a4bdc1a135ceba43b882712d375507
tejas-nitb <154320836+tejas-nitb@users.noreply.github.com>
2023-12-20 23:19:37 +0530

    Write a function that takes the binary representation of an unsigned integer and returns the number of '1' bits it has

---
+++ SetBits.java
@@ -0,0 +1,9 @@
+public static int hammingWeight(int num) 
+    {
+        int count = 0;
+        while (num > 0) {
+            count += num & 1;
+            num >>= 1;
+        }
+        return count;
+    }
