e1cca51c544259e9033cb791ad56bfd979a0cdc5
tejas-nitb <154320836+tejas-nitb@users.noreply.github.com>
2024-01-02 12:17:37 +0530

    Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.

---
+++ binary/FindMissingNumber.java
@@ -0,0 +1,8 @@
+public static int missingNumber(int[] nums) {
+        int n = nums.length;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans ^= nums[i] ^ (i+1);
+        }
+        return ans;
+    }
