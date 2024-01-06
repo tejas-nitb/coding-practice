f2c307b626bbe1be3d150d325491450dd4a97c8d
tejas-nitb <154320836+tejas-nitb@users.noreply.github.com>
2024-01-06 23:32:52 +0530

    You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.

---
+++ dynamic-programming/JumpGame.java
@@ -0,0 +1,6 @@
+public static boolean canJump(int[] nums) {  
+        int i = 0;
+        for (int reach = 0; i < nums.length && i <= reach; ++i)
+            reach = Math.max(i + nums[i], reach);
+        return i == nums.length;
+}
