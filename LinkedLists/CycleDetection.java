78590ca24fdf658d12eb427cf9e65b3c6a6208d6
tejas-nitb <154320836+tejas-nitb@users.noreply.github.com>
2024-01-09 20:13:57 +0530

    Given a singly linked list, find if the linked list is circular or not.

---
+++ LinkedLists/CycleDetection.java
@@ -0,0 +1,15 @@
+public boolean isCircular(Node head) { 
+        if (head == null) { 
+            return false; 
+        } 
+        Node slow = head; 
+        Node fast = head.next; 
+        while (fast != null && fast.next != null) { 
+            if (slow == fast) { 
+                return true; 
+            } 
+            slow = slow.next; 
+            fast = fast.next.next; 
+        } 
+        return false; 
+} 
