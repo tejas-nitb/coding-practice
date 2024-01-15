56cd23454e54b8482e227465731a358a84b0d226
tejas-nitb <154320836+tejas-nitb@users.noreply.github.com>
2024-01-15 16:45:51 +0530

    Update logic for duplicate check

--- Arrays/FindIfDuplicate.java
+++ Arrays/FindIfDuplicate.java
@@ -1,11 +1,11 @@
-public static boolean isDuplicate(int[] numbers) {
-    for (int index = 0; index < numbers.length; index++) {
-        int absIndex = Math.abs(numbers[index]);
-        if (numbers[absIndex] < 0) {
-            return true;
-        } else {
-            numbers[absIndex] = -numbers[absIndex];
+public static boolean findDuplicate(int[] arr) {
+        Set<Integer> integers = new HashSet<>();
+        for(Integer i: arr) {
+            if(integers.contains(i)) {
+                return true;
+            } else {
+                integers.add(i);
+            }
         }
-    }
-    return false;
+        return false;
 }
