6d300a98482443794a07f3aff7fd6bc80208fd8f
tejas-nitb <154320836+tejas-nitb@users.noreply.github.com>
2024-01-21 19:35:33 +0530

    Given the head of a singly linked list, reverse the list, and return the reversed list.

---
+++ leetcode-ace-75/ReverseLinkedListRecursive.java
@@ -0,0 +1,9 @@
+public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode rest = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return rest;
+}
